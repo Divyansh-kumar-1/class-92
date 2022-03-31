a342760ca187eded0dfd00ce55f16273a020abb5
Divyansh-kumar-1 <81742026+Divyansh-kumar-1@users.noreply.github.com>
2022-03-31 12:47:02 +0530

    Add files via upload

---
+++ game_login.js
@@ -0,0 +1,7 @@
+function addUser(){
+    player1_name= document.getElementById("Player1_name_input").Value;
+    player2_name=document.getElementById("Player2_name_input").Value;
+    localStorage.setItem("player1_name",player1_name);
+    localStorage.setItem("player2_name",player2_name);
+    window.location="game_page.html";
+}
